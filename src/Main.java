7c92dded973a7a3e4fdbfc505615e2f055997337
Aleph <19833891dr@gmail.com>
2025-03-21 16:14:07 +0500

    First publish

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+      boolean isHasCar = true;
+       if (isHasCar) 
+       {
+        System.out.println("Да верно?");
+       }
+       else
+       {
+        System.out.println("Жопа");
+       }
+    }
+}
